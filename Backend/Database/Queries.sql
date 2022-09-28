f6710e75ed8266068ea423e2c6e0efd6ffb1ea77
codemann0001 <ourique.cody@gmail.com>
2022-09-27 21:05:26 -0700

    user_login_data and hash_algorithm tables add with test query

--- Backend/Database/Queries.sql
+++ Backend/Database/Queries.sql
@@ -1,6 +1,16 @@
 USE Test_DB;
 
-DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS hash_algorithms;
+CREATE TABLE hash_algorithms (hash_ID int not null primary key auto_increment,
+algorithm_name varchar(40));
+
+DROP TABLE IF EXISTS user_login_data;
+CREATE TABLE user_login_data (ID int not null primary key auto_increment, 
+user_name varchar(40), user_password_hash varchar(250),
+user_password_salt varchar(100), foreign key (hash_id) references hash_algorithms(hash_ID),
+hash_id int not null, login_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, counter int);
+
+
 CREATE TABLE Items (
     ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     item_name varchar(256),
@@ -9,6 +19,7 @@ CREATE TABLE Items (
     item_quantity int
 );
 
+DROP TABLE IF EXISTS Items;
 -- -- I can't created Table "Table since "Table" is a reserved word.
 -- -- I created a table called "Restaurant_table"
 DROP TABLE IF EXISTS Restaurant_Table;
@@ -22,7 +33,9 @@ CREATE TABLE Orders (
 	ID int PRIMARY KEY AUTO_INCREMENT,
 	table_ID int NOT NULL,
     item_ID int NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_id int not null,
+    foreign key (user_id) references User_login_data(ID)
 );
 
 -- TEST QUERIES
@@ -43,17 +56,30 @@ VALUES
     (4)
 ;
 
-INSERT INTO Orders(table_ID, item_ID)
+INSERT INTO Orders(table_ID, item_ID, user_id)
 VALUES
-	(1,2),
-    (1,1),
-    (2,3),
-    (2,1),
-    (2,1),
-    (2,2),
-    (3,1),
-    (4,2)
+	(1,2, 1),
+    (1,1, 1),
+    (2,3, 1),
+    (2,1, 1),
+    (2,1, 1),
+    (2,2, 1),
+    (3,1, 1),
+    (4,2, 1)
 ;
+
+INSERT INTO user_login_data (user_name, user_password_hash, user_password_salt, hash_id, counter)
+VALUES
+("Cody", "some_hash_code1", "pseudorandom_string1", 1, 0), 
+("Chuong", "some_hash_code2", "pseudorandom_string2", 1, 0), 
+("Gabby", "some_hash_code3", "pseudorandom_string3", 2, 0)
+;
+
+INSERT INTO hash_algorithms (algorithm_name)
+VALUES
+("SHA-2"),
+("SHA-3");
+
 -- SELECT * FROM Items;
 -- SELECT * FROM Restaurant_table;
 -- SELECT * FROM Orders;
@@ -74,6 +100,26 @@ WHERE
     R.ID = O.table_ID
     AND
     R.table_number = 1
+;
+-- SELECT ALL ITEMS ORDERED BY TABLE 1 WITH USER ID
 
-
+SELECT
+	U.ID,
+	O.ID,
+	I.item_name, 
+    table_number,
+    created_at
+FROM 
+	user_login_data as U,
+	Items as I, 
+    Restaurant_table as R, 
+    Orders as O
+WHERE 
+	U.ID = O.user_id
+    AND
+	I.ID = O.item_ID 
+    AND
+    R.ID = O.table_ID
+    AND
+    R.table_number = 1
     
