40a94a34b1f4ae74e80948bce10223900d2c9dcb
Chuong Truong <chuongtruong@Brians-MacBook-Pro.local>
2022-10-24 11:04:52 -0700

    update get all items, add create order end point

--- Backend/application.py
+++ Backend/application.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask import render_template, request, redirect, make_response, jsonify
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 import db
 import views
 
 app = Flask(__name__)
+CORS(app)
 
 mysql = db.init(app)
 views.init(app, mysql)
--- Backend/views.py
+++ Backend/views.py
@@ -1,5 +1,5 @@
 from unicodedata import category
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, abort
 import json
 
 def init(app, mysql):
@@ -73,20 +73,88 @@ def init(app, mysql):
     def getAllItems():
         cur = mysql.connection.cursor()
         querry = """
-            SELECT I.*, P.photo
+            SELECT 
+                I.item_name,
+                I.item_description,
+                I.item_price,
+                I.item_quantity,
+                I.calories,
+                P.url,
+                C.name
             FROM Items I
-            JOIN Photos P
-            ON I.photoID=P.ID
+            JOIN Photos P ON I.photoID=P.ID
+            JOIN Categories C ON I.categoryID=C.ID
             ;
         """
-        print(querry)
+
         cur.execute(querry)
         result = cur.fetchall()
+        # print(result)
         json_data = []
+        print(len(result[0]))
         for i in result:
-            json_data.append(i[1])
-        return json.dumps(json_data)
+            print(i)
+            item = {
+                'name':i[0],
+                'desc':i[1],
+                'price':i[2],
+                'quantity':i[3],
+                'calories':i[4],
+                'url':i[5],
+                'category':i[6]
+            }
+
+            json_data.append(item)
+        return jsonify(json_data)
     
     #Create an order
-    
+    @app.route('/t=<table_no>/create', methods=['GET','POST'])
+    def createOrder(table_no):
+        if not table_no:
+            return abort(404, description="Invalid table")
 
+        cur = mysql.connection.cursor()
+
+        table_detail_querry = f"""
+            SELECT status
+            FROM Restaurant_Table
+            WHERE table_number = {int(table_no)}
+        """ 
+        print(table_detail_querry)
+        cur.execute(table_detail_querry)
+        table_status = cur.fetchall()[0][0] #statu 0: empty, 1: occupied
+        if table_status == 1:
+            return abort(404, description="The current table is occupied. Please select another table.")
+        
+        # # if result_table_detail
+
+        # # TODO: There can be error if table_no is invalid
+
+        if request.method == 'POST':
+            # https://dev.to/dev_elie/sending-data-from-react-to-flask-apm
+            
+            '''
+                frontend will send a object looks like
+                {
+                    "item_id": [item_id, item_id,...],
+                    "item_title: [item_title, item_title,...]"
+                    "quantity": [1,3,....]
+                }
+            '''
+
+            try:
+                item_id_list = request.json('item_id')
+                item_title_list = request.json('item_title')
+                quantity_list = request.json('quantity')                
+                create_order_querry = """
+                    INSERT INTO Orders(table)
+                    VALUE
+                """
+                for item_id, item_title,quantity in zip(item_id_list, item_title_list, quantity_list):
+                    tmp_sub_querry = f'(item_id,item_title,quanity),'
+                    create_order_querry += tmp_sub_querry
+                
+                create_order_querry
+
+            except Exception:
+                return abort(404, description='Error')
