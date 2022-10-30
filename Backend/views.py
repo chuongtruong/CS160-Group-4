9211392ae977f684ab0bc0c402dd7533b83391c5
Chuong Truong <chuongtruong@Brians-MacBook-Pro.local>
2022-10-30 14:50:11 -0700

    add socket.io, add more endpoint

--- Backend/views.py
+++ Backend/views.py
@@ -1,23 +1,74 @@
 from unicodedata import category
-from flask import Flask, render_template, request, jsonify, abort
-import json
+from flask import Flask, render_template, request, jsonify, abort, make_response
+from flask_cors import cross_origin
+import json, hashlib, secrets
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
-def init(app, mysql):
+def init(app, mysql, socketio):
     @app.route('/', methods=['GET', 'POST'])
     def index():
         if request.method == 'POST':
             # Fetch Form Data
             user_details = request.form
+
             employee_id = user_details['Employee ID Number']
             password = user_details['Password']
+
             cur = mysql.connection.cursor()
-            cur.execute("INSERT INTO users(employeeID, password) VALUES(%s,%s)",
+            cur.execute("INSERT INTO Employee(eID, password) VALUES(%s,%s)",
                         (employee_id, password))
 
             mysql.connection.commit()
             cur.close()
             return 'success'
+        
         return render_template('index.html')
+    
+    @app.route("/auth", methods=['POST', 'GET'])
+    @cross_origin(supports_credentials=True)
+    def authentication():
+        if request.method == 'POST':
+            data = json.loads(request.data)
+
+            employeeID = data.get('employeeID') # Extract employeeID
+            print(employeeID)
+            password = data.get('password') # Extract password
+            print(password)
+
+            encrypted = hashlib.sha1(password.encode('utf-8')).hexdigest()
+
+            cur = mysql.connection.cursor()
+            query = f'SELECT * FROM Employee WHERE eID = {employeeID} AND ' \
+                    f'password = "{encrypted}";'
+
+            result = cur.execute(query)
+
+            cur.close()
+
+            if result > 0:
+                return json.dumps({"employeeID": employeeID})
+
+            return json.dumps({"response": False})
+    
+    @app.route("/registration", methods=['POST', 'GET'])
+    @cross_origin(supports_credentials=True)
+    def registration():
+        if request.method == 'POST':
+
+            data = json.loads(request.data)
+
+            employeeID = data.get('employeeID')
+            password = data.get('password')
+            encrypted = hashlib.sha1(password.encode('utf-8')).hexdigest()
+
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO Employee(eID, password) VALUES(%s,%s)",
+                        (employeeID, encrypted))
+
+            mysql.connection.commit()
+            cur.close()
+
+        return {"response": "success"}
 
     @app.route('/users')
     def users():
@@ -107,54 +158,127 @@ def init(app, mysql):
             json_data.append(item)
         return jsonify(json_data)
     
-    #Create an order
-    @app.route('/t=<table_no>/create', methods=['GET','POST'])
-    def createOrder(table_no):
-        if not table_no:
-            return abort(404, description="Invalid table")
+    #CREATE A NEW ORDER
+    @app.route('/t=<table_id>/create', methods=['GET','POST'])
+    def createOrder(table_id):
+       
+        #check if table_id is valid
+        if not table_id: return abort(404, description="Missing table id in the request.")
 
         cur = mysql.connection.cursor()
+        check_id_querry = f"SELECT EXISTS(SELECT * FROM Restaurant_Table WHERE ID='{table_id}');"
+        cur.execute(check_id_querry)
+        result = cur.fetchall()
 
-        table_detail_querry = f"""
-            SELECT status
-            FROM Restaurant_Table
-            WHERE table_number = {int(table_no)}
-        """ 
-        print(table_detail_querry)
-        cur.execute(table_detail_querry)
-        table_status = cur.fetchall()[0][0] #statu 0: empty, 1: occupied
-        if table_status == 1:
-            return abort(404, description="The current table is occupied. Please select another table.")
-        
-        # # if result_table_detail
-
-        # # TODO: There can be error if table_no is invalid
+        if(result[0][0] == 0): return abort(404, description="Invalid table ID")
 
         if request.method == 'POST':
-            # https://dev.to/dev_elie/sending-data-from-react-to-flask-apm
+
+            get_a_new_order_no_querry = f"SELECT order_no FROM Orders ORDER BY order_no DESC LIMIT 1;"
+
+            cur.execute(get_a_new_order_no_querry)
+            result = cur.fetchall()
+            new_order_no = result[0][0] + 1
+            cur.close()
+
+            data = json.loads(request.data)
+            item_id_list = data['item_id']
+            quantity_list = data['quantity']
+            table_id = data['table_id']
+            create_a_new_order_querry = "INSERT INTO Orders(item_id, quantity, table_id, order_no)\n VALUE" 
+            for item_id, quantity in zip(item_id_list, quantity_list):
+                tmp_sub_querry = f'({item_id}, {quantity}, {table_id}, {new_order_no}),'
+                create_a_new_order_querry +=  '\n   ' + tmp_sub_querry
+
+            create_a_new_order_querry =create_a_new_order_querry[:-1] + '\n;'
             
-            '''
-                frontend will send a object looks like
-                {
-                    "item_id": [item_id, item_id,...],
-                    "item_title: [item_title, item_title,...]"
-                    "quantity": [1,3,....]
-                }
-            '''
-
             try:
-                item_id_list = request.json('item_id')
-                item_title_list = request.json('item_title')
-                quantity_list = request.json('quantity')                
-                create_order_querry = """
-                    INSERT INTO Orders(table)
-                    VALUE
-                """
-                for item_id, item_title,quantity in zip(item_id_list, item_title_list, quantity_list):
-                    tmp_sub_querry = f'(item_id,item_title,quanity),'
-                    create_order_querry += tmp_sub_querry
-                
-                create_order_querry
+                new_cur = mysql.connection.cursor()
+                new_cur.execute(create_a_new_order_querry)
+                mysql.connection.commit()
+                new_cur.close()
+                data = {'order_no': f'{new_order_no}', 'code': 'SUCCESS'}
+                return make_response(jsonify(data), 201)
+            except Exception as e:
+                return abort(404, description=f'Error. {e}')
+    
+    #UPDATE AN EXISTING ORDER
+    @app.route('/t=<table_id>/update', methods=['PATCH'])
+    def updateAnOrder(table_id):
+        if not table_id: return abort(404, description="Missing table id in the request.")
 
-            except Exception:
-                return abort(404, description='Error')
+        cur = mysql.connection.cursor()
+        check_id_querry = f"SELECT EXISTS(SELECT * FROM Restaurant_Table WHERE ID='{table_id}');"
+        cur.execute(check_id_querry)
+        result = cur.fetchall()
+
+        if(result[0][0] == 0): return abort(404, description="Invalid table ID")
+
+        update_an_order_querry = f""
+
+    #Get all items from a specific table_id
+    @app.route('/t=<table_id>/getAllItems', methods=['GET','POST'])
+    def getAllItemsByTableId(table_id):
+        cur = mysql.connection.cursor()
+
+        #check if table_id is an valid id in the Restaurant_tables table
+        check_id_querry = f"""
+            SELECT EXISTS(
+                SELECT * 
+                from Restaurant_Table 
+                WHERE ID='{table_id}');
+        """
+        cur.execute(check_id_querry)
+        result = cur.fetchall()
+
+        if(result[0][0] == 0):
+            return abort(404, description="Invalid table ID")
+        
+        get_all_items_by_table_id_querry = f"""
+            SELECT 
+                item_name,
+                quantity,
+                ROUND(item_price * quantity, 2) as total
+            FROM Orders
+            JOIN Items ON Orders.item_ID = Items.ID
+            WHERE Orders.table_ID = {table_id}
+            
+            ;
+        """
+        try:
+            cur.execute(get_all_items_by_table_id_querry)
+            result = cur.fetchall()
+            print(result)
+            json_data = []
+            for i in result:
+                item = {'name':i[0],
+                    'quantity':i[1],
+                    'total': i[2]
+                }
+                json_data.append(item)
+
+            return jsonify(json_data)
+        except Exception as e:
+            return abort(404, description=f'Error getting items by a specific table ID, {e}')
+
+    @socketio.on('message')
+    def message(data):
+        print(f'\n\n{data}\n\n')
+        send("I am the server")
+    
+    #store order history of each table
+    # cart_hitory = {
+    #   1: {
+    #      [item1, item 2, item 3] 
+    #   }
+    #   2: {
+    #       [item2, item5, item7]
+    #   }
+    # }
+    cart_history = {}
+
+    @socketio.on('join')
+    def on_join(data):
+        table = data['table']
+        items = data['item']
+        cart_history[table]
