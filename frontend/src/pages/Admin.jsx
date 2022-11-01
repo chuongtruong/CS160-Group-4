f0bc600e618dace6e7d30395aea27af797f8cfaa
JinHyuM3 <wenzhenhe1@gmail.com>
2022-10-31 19:15:50 -0700

    update

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,36 +1,57 @@
 import React,{useState,useEffect} from 'react'
 import { AdminMid } from '../components/admin/AdminMid'
 import { Navbar } from '../components/navbar/Navbar'
-import { menu } from '../components/Data'
 import { ListItems } from '../components/admin/ListItems'
-import { Modal } from '../components/modal/Modal'
+import { useParams } from 'react-router-dom'
+import { menu } from '../components/Data'
+import axios from 'axios'
 
-export const Admin = ({userId,isOpen,setIsOpen,setUserId}) => {
-  console.log(isOpen)
+export const Admin = () => {
+  const {id} = useParams()
   const [category,setCategory] = useState("Main")
   const [items,setItems] = useState(menu)
+  const [orderNum,setOrderNum] = useState(0)
+  const [customerItems,setCustomerItmes] = useState([])
+
+
+  // useEffect(() => {
+  //   const getAllMenus = async () => {
+  //     try {
+  //       const res = await axios.get("http://127.0.0.1:5000/menu")
+  //       setItems(res.data)
+  //     } catch(err) {
+  //       console.log(err)
+  //     }
+  //   }
+
+  //   getAllMenus()
+  // },[])
+
 
   useEffect(() => {
-    setItems(category === "Main" ? menu : menu.filter((item) => item.category === category))
+    setItems(category === "Main" ? items : items.filter((item) => item.category === category))
   },[category])
 
   return (
     <>
-      {isOpen === 'open' && <Modal setIsOpen={setIsOpen} setUserId={setUserId}/>}
       <div className="table-top">
-        <Navbar userId={userId} setIsOpen={setIsOpen}/>
+        <Navbar orderNum={orderNum} id={id}/>
         <AdminMid setCategory={setCategory}/>
       </div>
       <div className="table-bottom admin-bottom w-1/2 mt-10 mx-auto">
           {items?.map((item) => (
             <ListItems
               key={item.id}
-              title={item.title}
+              name={item.name}
               category={item.category}
               price={item.price}
-              cal={item.cal}
-              img={item.img}
+              calories={item.calories}
+              url={item.url}
               desc={item.desc}
+              orderNum={orderNum}
+              setOrderNum={setOrderNum}
+              customerItems={customerItems}
+              setCustomerItmes={setCustomerItmes}
             />
           ))}
       </div>
