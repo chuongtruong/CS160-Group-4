e468c1b265c2aef87793ab98cdde5e1baa3555cb
Chuong Truong <chuongtruong@Brians-MacBook-Pro.local>
2022-11-02 15:05:46 -0700

    add a diff ver of create order endpoint

--- Backend/views.py
+++ Backend/views.py
@@ -1,3 +1,4 @@
+from time import clock_getres
 from unicodedata import category
 from flask import Flask, render_template, request, jsonify, abort, make_response
 from flask_cors import cross_origin
@@ -163,7 +164,6 @@ def init(app, mysql, socketio):
     #CREATE A NEW ORDER
     @app.route('/t=<table_id>/create', methods=['GET','POST'])
     def createOrder(table_id):
-        # endpoint_url = 'localhost:5000/t={table_id}/create'
         """
         [
             {
@@ -200,9 +200,58 @@ def init(app, mysql, socketio):
             cur.close()
 
             data = json.loads(request.data)
+            print('\n\n\nDATA: ', data)
+
             item_id_list = data['item_id']
             quantity_list = data['quantity']
             table_id = data['table_id']
+            create_a_new_order_querry = "INSERT INTO Orders(item_id, quantity, table_id, order_no)\n VALUE" 
+            for item_id, quantity in zip(item_id_list, quantity_list):
+                tmp_sub_querry = f'({item_id}, {quantity}, {table_id}, {new_order_no}),'
+                create_a_new_order_querry +=  '\n   ' + tmp_sub_querry
+
+            create_a_new_order_querry =create_a_new_order_querry[:-1] + '\n;'
+            
+            try:
+                new_cur = mysql.connection.cursor()
+                new_cur.execute(create_a_new_order_querry)
+                mysql.connection.commit()
+                new_cur.close()
+                data = {'order_no': f'{new_order_no}', 'code': 'SUCCESS'}
+                return make_response(jsonify(data), 201)
+            except Exception as e:
+                return abort(404, description=f'Error. {e}')
+    
+    #CREATE A NEW ORDER
+    @app.route('/t=<table_id>/create2', methods=['GET','POST'])
+    def createOrder2(table_id):
+       
+        #check if table_id is valid
+        if not table_id: return abort(404, description="Missing table id in the request.")
+
+        cur = mysql.connection.cursor()
+        check_id_querry = f"SELECT EXISTS(SELECT * FROM Restaurant_Table WHERE ID='{table_id}');"
+        cur.execute(check_id_querry)
+        result = cur.fetchall()
+
+        if(result[0][0] == 0): return abort(404, description="Invalid table ID")
+
+        if request.method == 'POST':
+
+            get_a_new_order_no_querry = f"SELECT order_no FROM Orders ORDER BY order_no DESC LIMIT 1;"
+
+            cur.execute(get_a_new_order_no_querry)
+            result = cur.fetchall()
+            new_order_no = result[0][0] + 1
+            cur.close()
+
+            data = json.loads(request.data)
+
+            item_id_list, quantity_list = [], []
+            for key in data.keys():
+                item_id_list.append(int(key))
+                quantity_list.append(data[key]['quantity'])
+
             create_a_new_order_querry = "INSERT INTO Orders(item_id, quantity, table_id, order_no)\n VALUE" 
             for item_id, quantity in zip(item_id_list, quantity_list):
                 tmp_sub_querry = f'({item_id}, {quantity}, {table_id}, {new_order_no}),'
